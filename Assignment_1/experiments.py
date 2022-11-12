966e8423258cdddd13e0efea679de7b70734545a
Keyuan Wang <wangkeyuan1998@gmail.com>
2022-11-12 20:08:12 +0100

    Orthogonal sampling v.1 done

--- Assignment_1/experiments.py
+++ Assignment_1/experiments.py
@@ -1,37 +1,66 @@
-from mandelbrot import Monte_carlo_sampling, is_in
+from mandelbrot import Monte_carlo_sampling, Latin_hypercube_sampling, \
+    Orthogonal_Latin_hypercube_sampling, is_in
 import numpy as np
 from multiprocessing import Pool
 import pandas as pd
 
+# s_length = 100
+# s_start = 100
+# s_step = 100
+# s1 = s_length * s_step  # The biggest s value
+# s_list = np.arange(0, s_length) * s_step + s_start  # the list containing s values
+
+'''
+The Orthogonal sampling method from scipy can only work with sampling size = p**2,
+where p is a prime number. May apply a different method in future.
+'''
+# get prime numbers
+def primes_method(n):
+    out = list()
+    sieve = [True] * (n+1)
+    for p in range(2, n+1):
+        if (sieve[p] and sieve[p]%2==1):
+            out.append(p)
+            for i in range(p, n+1, p):
+                sieve[i] = False
+    return out
+
+s_list = np.array(primes_method(100)) ** 2
+s_list = np.delete(s_list, 0)
+s1 = s_list[-1]
+s_length = len(s_list)
+
 ite = 60
-s1 = int(1e4)
-s_length = 40
-
-# tune the s value to see the impact
-s_list = np.linspace(100, s1, s_length, dtype=int)
-
-# tune the i value to see the impact
+# tune the i (number of iterations) value to see the impact
 ite_list = np.arange(1, ite+1, dtype=int)
 
 
 def mandelbrot_sampling():
     '''i experiment'''
-    mand_area_i = np.zeros(ite)
+    mand_area_i = [np.zeros(ite), np.zeros(ite), np.zeros(ite)]
     for i in range(ite):
         # initialize the sampling points (random sampling)
-        c = Monte_carlo_sampling(s1)
+        c_MC = Monte_carlo_sampling(s1, circle='N')
+        c_LHS = Latin_hypercube_sampling(s1)
+        c_OS = Orthogonal_Latin_hypercube_sampling(s1)
+        C = [c_MC, c_LHS, c_OS]
         # compute the mandelbrot set area
-        mand_area = (is_in(c, ite_list[i]).sum()/s1) * np.pi * 4
-        mand_area_i[i] = mand_area
+        for j, c in enumerate(C):
+            mand_area = (is_in(c, ite_list[i]).sum()/s1) * np.pi * 4
+            mand_area_i[j][i] = mand_area
 
     '''s experiment'''
-    mand_area_s = np.zeros(s_length)
+    mand_area_s = [np.zeros(s_length), np.zeros(s_length), np.zeros(s_length)]
     for i in range(s_length):
         # initialize the sampling points (random sampling)
-        c = Monte_carlo_sampling(s_list[i])
+        c_MC = Monte_carlo_sampling(s_list[i], circle='N')
+        c_LHS = Latin_hypercube_sampling(s_list[i])
+        c_OS = Orthogonal_Latin_hypercube_sampling(s_list[i])
+        C = [c_MC, c_LHS, c_OS]
         # compute the mandelbrot set area
-        mand_area = (is_in(c, ite).sum()/s_list[i]) * np.pi * 4
-        mand_area_s[i] = mand_area
+        for j, c in enumerate(C):
+            mand_area = (is_in(c, ite).sum()/s_list[i]) * np.pi * 4
+            mand_area_s[j][i] = mand_area
 
     return mand_area_i, mand_area_s
 
@@ -41,23 +70,33 @@ if __name__ == '__main__':
 
     # repeat experiments for 10 times (stochasticity)
     repetition = 10
-    MAND_AREA = pool.starmap(mandelbrot_sampling, [()for _ in range(repetition)])
+    MAND_AREA = pool.starmap(mandelbrot_sampling, [() for _ in range(repetition)])
+    # Note the shape of MAND_AREA: a 3d list, the first dimension: [rep_1, rep_2, ..., rep_n],
+    # the second dimension: (mand_area_i, mand_area_s),
+    # the thrid dimension: (area_MC, area_LHS, area_OS).
+    # That is:
+    # MAND_AREA_MC = MAND_AREA[:][:][0]
+    # MAND_AREA_LHS = MAND_AREA[:][:][1]
+    # MAND_AREA_OS = MAND_AREA[:][:][2]
+
+    s_methods = ['MC', 'LHS', 'OS']
 
     df_ite = pd.DataFrame(columns=['iteration', 'area', 'repetition'])
     df_sampling = pd.DataFrame(columns=['num_s', 'area', 'repetition'])
 
     for i in range(repetition):
-        df_i = pd.DataFrame({'iteration': ite_list, \
-                             'area': MAND_AREA[i][0], \
-                             'repetition': np.repeat(i+1, ite)})
-        
-        df_ite = pd.concat([df_ite, df_i], ignore_index=True)
+        for j in range(len(s_methods)):
+            df_i = pd.DataFrame({'iteration': ite_list, \
+                                'area': MAND_AREA[i][0][j], \
+                                'repetition': np.repeat(i+1, ite), \
+                                's_method': [s_methods[j]] * ite})
+            df_ite = pd.concat([df_ite, df_i], ignore_index=True)
 
-        df_s = pd.DataFrame({'num_s': s_list, \
-                             'area': MAND_AREA[i][1], \
-                             'repetition': np.repeat(i+1, s_length)})
-
-        df_sampling = pd.concat([df_sampling, df_s], ignore_index=True)
+            df_s = pd.DataFrame({'num_s': s_list, \
+                                'area': MAND_AREA[i][1][j], \
+                                'repetition': np.repeat(i+1, s_length), \
+                                's_method': [s_methods[j]] * s_length})
+            df_sampling = pd.concat([df_sampling, df_s], ignore_index=True)
 
     df_ite.to_csv('test_i.csv', index=False)
     df_sampling.to_csv('test_s.csv', index=False)
--- Assignment_1/mandelbrot.py
+++ Assignment_1/mandelbrot.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numpy import random
+from scipy.stats import qmc
 
 def is_in(c, num_iterations):
     z = 0.0
@@ -9,11 +10,43 @@ def is_in(c, num_iterations):
     return abs(z) <= 2   # True or False?
 
 
-def Monte_carlo_sampling(num, R=2):
-    r = R * np.sqrt(random.random_sample(size=num))  # random radius
-    theta = random.random_sample(size=num) * 2 * np.pi  # random angel
+def Monte_carlo_sampling(num, R=2, circle='Y'): # Monte Carlo sampling in a circle
+    if circle == 'Y':
+        r = R * np.sqrt(random.random_sample(size=num))  # random radius
+        theta = random.random_sample(size=num) * 2 * np.pi  # random angel
 
-    # convert to Cartesian coordinates and map to a complex plane
-    c_array = r * np.cos(theta) + 1j * r * np.sin(theta)
+        # convert to Cartesian coordinates and map to a complex plane
+        c_array = r * np.cos(theta) + 1j * r * np.sin(theta)
+
+    else:
+        x = 4 * random.random_sample(size=num) - 2  # scale from [0, 1] to [-2, 2]
+        y = 4 * random.random_sample(size=num) - 2  # scale from [0, 1] to [-2, 2]
+
+        c_array = x + 1j * y
+
+    return c_array
+
+def Latin_hypercube_sampling(num):
+    l_bounds= [-2, -2]
+    u_bounds = [2, 2]
+
+    # Plain Latin hypercube sampling
+    sampler = qmc.LatinHypercube(d=2, strength=1)
+    c_array = sampler.random(n=num)
+    c_array = qmc.scale(c_array, l_bounds, u_bounds)
+    c_array = c_array[:, 0] + 1j * c_array[:, 1]
+
+    return c_array
+
+def Orthogonal_Latin_hypercube_sampling(num):
+    l_bounds= [-2, -2]
+    u_bounds = [2, 2]
+
+    # orthogonal-array-based Latin hypercube sampling
+    # Note! The number can only be a square of prime number!
+    sampler = qmc.LatinHypercube(d=2, strength=2)
+    c_array = sampler.random(n=num)
+    c_array = qmc.scale(c_array, l_bounds, u_bounds)
+    c_array = c_array[:, 0] + 1j * c_array[:, 1]
 
     return c_array
